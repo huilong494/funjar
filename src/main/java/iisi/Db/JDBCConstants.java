bd4302736ea098e37f28bb1a50ae4112c7a64ab1
huilong494 <user@1607016NB01.iead.local>
2016-11-16 17:16:52 +0800

    莫名其妙

--- src/main/java/iisi/Db/JDBCConstants.java
+++
@@ -1,9 +0,0 @@
-package iisi.Db;
-
-public interface JDBCConstants {
-//存放連線的基本資料
-	String Driver="com.mysql.jdbc.Driver";  
-	String URI="jdbc:mysql://localhost/scott?useSSL=false";
-	String USERNAME="root";
-	String PASSWORD="iisi";
-}
---
+++ src/main/java/org/iisi/Bean/RemainHour.java
@@ -0,0 +1,33 @@
+package org.iisi.Bean;
+public class RemainHour {
+	private float remainHour;
+	private String kname;
+	
+	public RemainHour(float remainHour, String kname) {
+		this.remainHour = remainHour;
+		this.kname = kname;
+		
+
+	}
+
+	public float getremainHour() {
+		return remainHour;
+
+	}
+
+	public String getkname() {
+		return kname;
+
+	}
+
+	
+	public void setremainHour(float remainHour) {
+		this.remainHour = remainHour;
+
+	}
+
+	public void setkname(String kname) {
+		this.kname = kname;
+
+	}
+}
